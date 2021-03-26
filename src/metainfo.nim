8c0168e3a3cc26c1c2101550d579225cf7c5bb88
Dmitry Ponyatov <dponyatov@gmail.com>
2021-03-26 16:41:51 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

---
+++ src/metainfo.nim
@@ -0,0 +1,8 @@
+const
+    MODULE*  = "IDE"
+    TITLE*   = ""
+    AUTHOR*  = "Dmitry Ponyatov"
+    EMAIL*   = "dponyatov@gmail.com"
+    YEAR*    = 2020
+    LICENSE* = "MIT"
+    GITHUB*  = "https://github.com/ponyatov/IDE"
